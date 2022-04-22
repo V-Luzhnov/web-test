7663d40aedfacd9dee7422e443f23287247a32d9
Vitalii Luzhnov <vitalii@luzhnov.pro>
2022-04-22 14:49:17 +0300

    hw 6

--- src/test/java/org/hw5/MaviTest.java
+++ src/test/java/org/hw5/MaviTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 
 import java.io.IOException;
 
